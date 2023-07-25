84e3c7b1a371a41386ee287f022167af6d53144f
duonghiep416 <duonghiep416@gmail.com>
2023-07-25 22:55:22 +0700

    Update

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
     return (
         <footer>
             <Container>
                 <Row>
-                    <Col className="text-center py-3 " > Copyright &copy; Vvorkshop</Col>
+                    <Col className="text-center py-3 ">
+                        {" "}
+                        Copyright &copy; IT0502
+                    </Col>
                 </Row>
             </Container>
-
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
